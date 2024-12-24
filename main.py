3662f8a8e6f84aa606e9d5b2842f448af45fe3ed
TreasureCoded-locked-in <akpannsikak410@gmail.com>
2024-12-24 19:40:42 +0100

    First Commit

---
+++ main.py
@@ -0,0 +1,8 @@
+setx = {'green', 'red', 'purple'}
+sety = {'red', 'yellow', 'purple'}
+print('Original set elements:')
+print(setx)
+print(sety)
+print('\nIntersection of two said sets:')
+setz = setx.intersection(sety)
+print(setz)
